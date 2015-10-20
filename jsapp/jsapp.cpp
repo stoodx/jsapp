5664ff3500b03297df589183a89d36c95d631387
stoodx <siewsoft@gmail.com>
2015-10-20 09:35:32 +0300

    Added Duktape to the project

--- jsapp/jsapp.cpp
+++ jsapp/jsapp.cpp
@@ -1,11 +1,23 @@
 // jsapp.cpp : Defines the entry point for the console application.
 //
-
 #include "stdafx.h"
+#include <iostream>
 
+extern "C" {
+#include "duktape.h"
+}
 
 int _tmain(int argc, _TCHAR* argv[])
 {
+	duk_context* ctx = duk_create_heap_default();
+	if(!ctx)
+	{
+		std::cout << "Fail duk_create_heap_default()" << std::endl; 
+		return 1;
+	}
+
+	duk_destroy_heap(ctx);
+	std::system("pause");
 	return 0;
 }
 
